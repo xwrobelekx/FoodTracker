ff54c73ba2ca4a38cc7fc8a636c58995f4118016
Kamil Wrobel <wrobelk@live.com>
2018-08-10 19:50:32 -0400

    stars are now set up and can be highlighted (there is one error but its not affecting the app)

--- FoodTracker/RaitingControl.swift
+++ FoodTracker/RaitingControl.swift
@@ -8,13 +8,27 @@
 
 import UIKit
 
-class RaitingControl: UIStackView {
+@IBDesignable class RaitingControl: UIStackView {
     
     //MARK: Properties
     
     private var ratingButtons = [UIButton]()
+    var rating = 0 {
+        didSet {
+            updateButtonSelectionStates()
+        }
+    }
     
-    var rating = 0
+    @IBInspectable var starSize: CGSize = CGSize(width: 44.0, height: 44.0) {
+        didSet {
+            setupButton()
+        }
+    }
+    @IBInspectable var starCount: Int = 5 {
+        didSet {
+            setupButton()
+        }
+    }
     
 
     //MARK: Initalization
@@ -32,7 +46,21 @@ class RaitingControl: UIStackView {
     //MARK: Button Action
     // in order for the  button to be created (#selector) the "@objc" was added by compailer in front of this function
     @objc func ratingButtonTapped(button: UIButton) {
-        print("button pressed 👍🏻")
+        guard let index = ratingButtons.index(of: button) else {
+            fatalError("The button, \(button), is not in the ratingButtons array: \(ratingButtons)")
+        }
+        
+        // calculate the rating of selected button
+        let selectedRating = index + 1
+        
+        if selectedRating == rating {
+            // If the selected star represents the current rating, reset the rating to 0.
+            rating = 0
+        } else {
+            // Otherwise set the rating to the selected star
+            rating = selectedRating
+        }
+        
     }
     
     
@@ -41,16 +69,33 @@ class RaitingControl: UIStackView {
     //MARK: Private Methods
     
     private func setupButton() {
+    
+        // clear any existing buttons
+        for button in ratingButtons {
+            removeArrangedSubview(button)
+            button.removeFromSuperview()
+        }
+        ratingButtons.removeAll()
         
-        for _ in 0..<5 {
+        //load button images
+        let bundle = Bundle(for: type(of: self))
+        let filledStar = UIImage(named: "filledStar", in: bundle, compatibleWith: self.traitCollection)
+        let highlightedStar = UIImage(named: "highlightedStar", in: bundle, compatibleWith: self.traitCollection)
+        let emptyStar = UIImage(named: "emptyStar", in: bundle, compatibleWith: self.traitCollection)
+        
+        
+        for _ in 0..<starCount {
             // create button
             let button = UIButton()
-            button.backgroundColor = UIColor.red
+            button.setImage(emptyStar, for: .normal)
+            button.setImage(filledStar, for: .selected)
+            button.setImage(highlightedStar, for: .highlighted)
+            button.setImage(highlightedStar, for: [.highlighted, .selected])
             
             // add constraints
             button.translatesAutoresizingMaskIntoConstraints = false
-            button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
-            button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+            button.heightAnchor.constraint(equalToConstant: starSize.height).isActive = true
+            button.widthAnchor.constraint(equalToConstant: starSize.width).isActive = true
             
             //set up the button action
             button.addTarget(self, action: #selector(RaitingControl.ratingButtonTapped(button:)), for: .touchUpInside)
@@ -63,6 +108,36 @@ class RaitingControl: UIStackView {
             ratingButtons.append(button)
         }
         
+        updateButtonSelectionStates()
+        
+    }
+    
+    private func updateButtonSelectionStates() {
+        
+        for (index, button) in ratingButtons.enumerated() {
+            
+            // If the index of a button is less than the rating, that button should be selected.
+            button.isSelected = index < rating
+        }
+        
+        
     }
 
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
