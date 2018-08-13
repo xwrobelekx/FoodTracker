f7c4452972714d067bb9baa33e537a763e7cfe27
Kamil Wrobel <wrobelk@live.com>
2018-08-13 10:41:47 -0400

    At this point, the custom table view controller subclass, MealTableViewController, has a mutable array that’s pre-populated with some sample meals. Now you need to display this data in the user interface.

---
+++ FoodTracker/MealTableViewCell.swift
@@ -0,0 +1,32 @@
+//
+//  MealTableViewCell.swift
+//  FoodTracker
+//
+//  Created by Kamil Wrobel on 8/13/18.
+//  Copyright © 2018 Kamil Wrobel. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    //MARK: Properties
+    
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var photoImageView: UIImageView!
+    @IBOutlet weak var ratingControll: RaitingControl!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
