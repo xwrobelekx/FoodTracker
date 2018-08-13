83e1b20d4fc521fb8c74d16088ada27a831eb31a
Kamil Wrobel <wrobelk@live.com>
2018-08-13 12:05:32 -0400

    noticed that the custom cell is not aligned right, but the lesson didn't say anything about the constraints, moved the image in the call to be aligned with the margin also cleaned up some code

--- FoodTracker/MealViewController.swift
+++ FoodTracker/MealViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  MealViewController.swift
 //  FoodTracker
 //
 //  Created by Kamil Wrobel on 8/9/18.
@@ -8,12 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+class MealViewController: UIViewController, UITextFieldDelegate, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     //MARK: Properties
     
     @IBOutlet weak var nameTextField: UITextField!
-    @IBOutlet weak var mealNameLabel: UILabel!
+
     @IBOutlet weak var photoImageView: UIImageView!
     @IBOutlet weak var ratingControl: RaitingControl!
     
@@ -34,7 +34,7 @@ class ViewController: UIViewController, UITextFieldDelegate, UIImagePickerContro
     }
     
     func textFieldDidEndEditing(_ textField: UITextField) {
-        mealNameLabel.text = textField.text
+        
     }
     //MARK: UIImagePickerControllerDelegate
     
